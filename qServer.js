f256a5bcd5a877d662bebd403dd5d23725efe968
mbhamalimosaic <mbhamalimosaic@gmail.com>
2015-10-17 20:48:59 -0400

    From codeanywhere

--- qServer.js
+++ qServer.js
@@ -16,76 +16,78 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded());
 //app.use(cookieParser('my secret here'));
 
-app.use(bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-    extended: true
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
 }));
 
 //REST for qMaster client app
 app.post('/qMaster', function (req, res){
-    var id = req.body.id;
-    qMaster.getMyques(id, function (param){
-        res.send(param);
-    })
+    var id = req.body.id;
+    qMaster.getMyques(id, function (param){
+        res.send(param);
+    })
 })
 
 //CHECKED!!!
 app.post('/qMaster/register', function (req, res){
-    var user={qService:req.body.service
-        ,cellNum:req.body.cellnum
-        ,location:{lat:req.body.lat,lng:req.body.lng}
-        ,fullName:req.body.name};
+    var user={qService:req.body.service
+        ,cellNum:req.body.cellnum
+        ,location:{lat:req.body.lat,lng:req.body.lng}
+        ,fullName:req.body.name};
 
-    qMaster.register(user, function (param){
-        console.log("New qMaster successfully registered: ");
-        console.log(param);
-        res.send(param);
-    });
+    qMaster.register(user, function (param){
+        console.log("New qMaster successfully registered: ");
+        console.log(param);
+        res.send(param);
+    });
 });
 //CHECKED!!!
 app.post('/qMaster/createQ', function (req, res){
-    var qmaster = {cellNum:req.body.cellnum};
+    var qmaster = {cellNum:req.body.cellnum};
 
-    var vQ = {
-      qActive:false,
-      qFrame:req.body.frame,
-      qName:req.body.qName,
-      qTag: req.body.tag
-    }
+    var vQ = {
+      qActive:false,
+      qFrame:req.body.frame,
+      qName:req.body.qName,
+      qAlphanum:req.body.alphanum,
+      qLastCode:1,
+      qTag: req.body.tag
+    }
 
-    qMaster.createQ(qmaster,vQ, function (param){
-        console.log("Q- " + param + " successfully Created!");
-        res.send(param);
-    })
+    qMaster.createQ(qmaster,vQ, function (param){
+        console.log("Q- " + param + " successfully Created!");
+        res.send(param);
+    });
 });
 
 app.post('/qMaster/destroyQ', function (req,res){
-    var user = {cellNum:req.body.cellnum};
+    var user = {cellNum:req.body.cellnum};
 
-    qMaster.destroyQ(user, function (param){
-        console.log("Q- " + param + " obliterated!!!");
-        res.send(param);
-    });
+    qMaster.destroyQ(user, function (param){
+        console.log("Q- " + param + " obliterated!!!");
+        res.send(param);
+    });
 });
 
 app.post('/qMaster/updates', function (req,res){
-    var user = {cellNum:req.body.cellnum};
+    var user = {cellNum:req.body.cellnum};
 
-    qMaster.update(user, function (param){
-        console.log("Q- " + param + " obliterated!!!");
-        res.send(param);
-    });
+    qMaster.update(user, function (param){
+        console.log("Q- " + param + " obliterated!!!");
+        res.send(param);
+    });
 });
 
 app.post('/qMaster/deQUser', function (req,res){
-    var user = req.body.ucellnum;
-    var dqTime = Date.now();
-    var Qmaster = {cellNum:req.body.mcellnum};
+    var user = req.body.ucellnum;
+    var dqTime = Date.now();
+    var Qmaster = {cellNum:req.body.mcellnum};
 
-    qMaster.dQUser(Qmaster,user, function (param){
-        console.log("Q- " + param + " obliterated!!!");
-        res.send(param);
-    });
+    qMaster.dQUser(Qmaster,user, function (param){
+        console.log("Q- " + param + " obliterated!!!");
+        res.send(param);
+    });
 });
 
 //REST for user client app
@@ -94,54 +96,54 @@ app.post('/user', function (req, res){
 });
 //CHECKED!!!
 app.post('/user/register', function (req, res){
-    var user = {qAlias:req.body.alias,cellNum:req.body.cellnum};
+    var user = {qAlias:req.body.alias,cellNum:req.body.cellnum};
 console.log(user);
-    qEr.QEr(user, function (param){
-        console.log("New qEr registered- " + param);
-        res.send("You have been registered");
-    });
+    qEr.QEr(user, function (param){
+        console.log("New qEr registered- " + param);
+        res.send("You have been registered");
+    });
 });
 
 //CHECKED!!!
 app.post('/user/joinQ', function (req, res){
-    var uid = req.body.uid
-       ,qid = req.body.qid;
-    qEr.joinQ(uid, qid, function (param){
-        console.log("QEr- '" + uid + "' joined Q: '" + qid + "'!");
-        res.send(param);
-    });
+    var uid = req.body.uid
+       ,qid = req.body.qid;
+    qEr.joinQ(uid, qid, function (param){
+        console.log("QEr- '" + uid + "' joined Q: '" + qid + "'!");
+        res.send(param);
+    });
 
 });
 //CHECKED!!!
 app.post('/user/availQs', function (req, res){
-    qEr.allQs(function(param){
-    console.log("Available Qs are: '" + param + "'...")
-        res.send(param);
-    console.log(param);
-    });
+    qEr.allQs(function(param){
+    console.log("Available Qs are: '" + param + "'...")
+        res.send(param);
+    console.log(param);
+    });
 });
 
 app.post('/user/leaveQ', function (req, res){
-    var uid = req.body.uid
-        ,qid = req.body.qid;
+    var uid = req.body.uid
+        ,qid = req.body.qid;
 
-    qEr.leaveQ(uid, qid, function (param){
-        console.log("QEr:'" + uid + "' has left Q:" + qid + "...");
-        res.send(param);
-    })
+    qEr.leaveQ(uid, qid, function (param){
+        console.log("QEr:'" + uid + "' has left Q:" + qid + "...");
+        res.send(param);
+    })
 });
 //CHECKED!!!
 app.post('/user/joinedQs', function (req, res){
-    var uid = req.body.uid;
+    var uid = req.body.uid;
 
-    qEr.joinedQ(uid, function (param){
-        console.log("List of Qs joined by: " + uid + ":")
-        res.send(param);
-    });
+    qEr.joinedQ(uid, function (param){
+        console.log("List of Qs joined by: " + uid + ":")
+        res.send(param);
+    });
 });
 
 
 var server = app.listen(3000, function(){
-    console.log("listening for Q requests on port 3000!");
+    console.log("listening for Q requests on port 3000!");
 
 });
