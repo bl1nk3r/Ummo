5122d0e8a556a5ffae04e356cc4e1887e774c619
bl1nk3r <jay.rego.14@gmail.com>
2015-11-08 14:44:59 +0200

    Server fully integrated with Mongo datastorage and each function tested; server code clean up and introduction of Process Manager 'pm2'

--- Server/qMaster.js
+++ Server/qMaster.js
@@ -1,27 +1,31 @@
-function register(qMaster,callback) {
-	var fireB = new Firebase('https://ummo.firebaseio.com/qMaster/users/'+qMaster.cellNum);
+/***************************************************************************************************
+*************************************UMMO QMASTER & RELATED FUNCTIONS*******************************
+***************************************************************************************************/
+
+function register(qMaster, callback) {
+	var fireB = new Firebase('https://ummo.firebaseio.com/qMaster/users/' + qMaster.cellNum);
 	fireB.set(qMaster);
 }
 
-function craeteQ(qMaster,vQ,callback){
-	var fireB = new Firebase('https://ummo.firebaseio.com/qMaster/users/'+qMaster.cellNum+"/managedQ");
+function createQ(qMaster, vQ, callback) {
+	var fireB = new Firebase('https://ummo.firebaseio.com/qMaster/users/' + qMaster.cellNum + "/managedQ");
 	fireB.set(vQ);
 
 }
 
-function destroyQ(qMaster,callback) {
-	var fireB = new Firebase('https://ummo.firebaseio.com/qMaster/users/'+qMaster.cellNum+"/managedQ");
+function destroyQ(qMaster, callback) {
+	var fireB = new Firebase('https://ummo.firebaseio.com/qMaster/users/' + qMaster.cellNum + "/managedQ");
 	fireB.set(null);
 
 }
 
-function dQUser(qMaster,user) {
+function dQUser(qMaster, user) {
 	console.log(user);
-	var fireB = new Firebase('https://ummo.firebaseio.com/qMaster/users/'+qMaster.cellNum+"/managedQ/qErs");
-	fireB.once("value",function (snapshot) {
+	var fireB = new Firebase('https://ummo.firebaseio.com/qMaster/users/' + qMaster.cellNum + "/managedQ/qErs");
+	fireB.once("value", function (snapshot) {
 		if(fireB.child(user)){
-		var ttdq=new Firebase('https://ummo.firebaseio.com/qMaster/users/'+qMaster.cellNum+"/managedQ/ttdq");
-		var	dqTime = new Firebase('https://ummo.firebaseio.com/qMaster/users/'+qMaster.cellNum+"/managedQ/");
+		var ttdq=new Firebase('https://ummo.firebaseio.com/qMaster/users/' + qMaster.cellNum + "/managedQ/ttdq");
+		var	dqTime = new Firebase('https://ummo.firebaseio.com/qMaster/users/' + qMaster.cellNum + "/managedQ/");
 		dqTime.once("value",function(snap){
 			if (snap.child("dqTime").exists()) {
 				console.log(snap.child("dqTime").val());
@@ -29,18 +33,18 @@ function dQUser(qMaster,user) {
 			}
 				dqTime.child("dqTime").set(Date.now());
 
-		})
+		});
 
-	 var pos=snapshot.child(user).val();
-	 snapshot.forEach(function(childSnapshot) {
-    var key = childSnapshot.key();
-    var childData = childSnapshot.val();
-    if (childData>pos) {
-    	fireB.child(key).set(childData-1);
-    }
+	var pos=snapshot.child(user).val();
+	snapshot.forEach(function(childSnapshot) {
+	    var key = childSnapshot.key();
+	    var childData = childSnapshot.val();
+	    if (childData > pos) {
+	    	fireB.child(key).set(childData-1);
+	    }
 
-    fireB.child(user).set(null);
-  });
+    	fireB.child(user).set(null);
+  	});
 }
 });
 }
@@ -62,15 +66,15 @@ function destroyAllQs() {
 function update(qMaster,callback) {
 	var fireB = new Firebase('https://ummo.firebaseio.com/qMaster/users/'+qMaster.cellNum);
 	fireB.on("value",function (snapshot) {callback(snapshot.val())},function (err) {callback(err)});
-	}
+}
 
 
-exports.createQ=craeteQ;
-exports.register=register;
-exports.destroyQ=destroyQ;
-exports.dQUser=dQUser;
-exports.moveUser=moveUser;
-exports.getFeedbacks=getFeedbacks;
+exports.createQ = createQ;
+exports.register = register;
+exports.destroyQ = destroyQ;
+exports.dQUser = dQUser;
+exports.moveUser = moveUser;
+exports.getFeedbacks = getFeedbacks;
 exports.getMyques = getMyques;
-exports.destroyAllQs=destroyAllQs;
-exports.update=update;
+exports.destroyAllQs = destroyAllQs;
+exports.update = update;
--- Server/qServer.js
+++ Server/qServer.js
@@ -1,3 +1,7 @@
+/***************************************************************************************************
+*************************************UMMO Server: Consolidating QER && QMASTER**********************
+***************************************************************************************************/
+
 /**
  * Created by sihle on 2015/07/04.
  */
@@ -5,13 +9,27 @@
  /**
  * Edited by blinker on 2015/07/08
  */
-var express = require('express');
-var app = express();
-var url = require("url");
-var qMaster = require("./qMaster");
-var qEr = require("./user");
+var express = require('express')
+    ,app = express()
+    ,url = require("url")
+    ,qMaster = require("./qMaster")
+    ,qEr = require("./qEr")
+    ,config = require('./config');
 
-var bodyParser = require('body-parser')
+var qMasterCategory = "",
+    qMasterCellNum = 0,
+    qMasterService = "";
+
+//include access to the MongoDB driver for Node
+var mongojs = require("mongojs") 
+//localhost specified       
+    ,host = "127.0.0.1" 
+//use the default port for Mongo server/client connections          
+    ,port = "27017"             
+//init Ummo (database) and Objectives (collection)
+    ,db = mongojs("ummodb", ["qEr", "qMaster", "categories", "queues"]);
+
+var bodyParser = require('body-parser');
 //cookieParser = require('cookie-parser');
 app.use(bodyParser.urlencoded());
 //app.use(cookieParser('my secret here'));
@@ -31,38 +49,75 @@ app.post('/qMaster', function (req, res){
 
 //CHECKED!!!
 app.post('/qMaster/register', function (req, res){
-    var user={qService:req.body.service
-        ,cellNum:req.body.cellnum
-        ,location:{lat:req.body.lat,lng:req.body.lng}
-        ,fullName:req.body.name};
+    var user = {qService:req.body.service
+                ,cellNum:req.body.cellnum
+                ,location:{lat:req.body.lat,lng:req.body.lng}
+                ,fullName:req.body.name
+                ,category:req.body.category};
+
+/*************************Insertion of new QMaster to MongoDB (works well)************************/
+    db.qMaster.insert(user, function (err, doc) {
+            if (err){
+                console.log("err");
+            }
+            else{
+                console.log("New qMaster successfully registered: ");
+                res.json(doc);
+            }
+        });
 
     qMaster.register(user, function (param){
-        console.log("New qMaster successfully registered: ");
+
+        
         console.log(param);
         res.send(param);
     });
 });
-//CHECKED!!!
+
 app.post('/qMaster/createQ', function (req, res){
     var qmaster = {cellNum:req.body.cellnum};
 
     var vQ = {
       qActive:false,
       qFrame:req.body.frame,
-      qName:req.body.qName,
-      qTag: req.body.tag
+      qName:req.body.qname,
+      qTag: req.body.tag,
+      qRequirements: req.body.qreq,
+      qMasCellNum: req.body.cellnum
     }
 
-    qMaster.createQ(qmaster,vQ, function (param){
+/*************************Creation of new Queue in MongoDB (works well)************************/
+    db.queues.insert(vQ, function (err, doc) {
+        if (err){
+            console.log("err");
+        }
+        else{
+            console.log("New queue successfully registered: ");
+            res.json(doc);
+        }
+    });
+
+    qMaster.createQ(qmaster, vQ, function (param){
         console.log("Q- " + param + " successfully Created!");
         res.send(param);
     })
 });
 
 app.post('/qMaster/destroyQ', function (req,res){
-    var user = {cellNum:req.body.cellnum};
+    var cellNum = req.body.cellnum;
 
-    qMaster.destroyQ(user, function (param){
+    //DOES A PHANTOM "REMOVE"... NEEDS RETESTING!!!
+    db.queues.remove({"qMasCellNum":cellNum}, function (err, doc) {
+        if (err){
+            console.log("err");
+        } 
+        else{
+            console.log("Queue successfully deleted: ");
+            res.json(doc);
+        } 
+    });
+
+    qMaster.destroyQ(cellNum, function (param){
         console.log("Q- " + param + " obliterated!!!");
         res.send(param);
     });
@@ -88,41 +143,81 @@ app.post('/qMaster/deQUser', function (req,res){
     });
 });
 
-//REST for user client app
-app.post('/user', function (req, res){
+//REST for qEr client app
+app.post('/qEr', function (req, res){
 
 });
-//CHECKED!!!
-app.post('/user/register', function (req, res){
-    var user = {qAlias:req.body.alias,cellNum:req.body.cellnum};
-console.log(user);
-    qEr.QEr(user, function (param){
+
+app.post('/qEr/register', function (req, res){
+
+    var qer = {qAlias:req.body.alias, cellNum:req.body.cellnum};
+
+/*************************Registration of a new QEr in MongoDB (works well)************************/
+    db.qEr.insert(qer, function (err, doc) {
+        if (err){
+            console.log("err");
+        }
+        else{
+            console.log("New qEr successfully registered: ");
+            res.json(doc);
+        }
+    });
+
+    qEr.QEr(qer, function (param){
         console.log("New qEr registered- " + param);
         res.send("You have been registered");
     });
 });
 
-//CHECKED!!!
-app.post('/user/joinQ', function (req, res){
-    var uid = req.body.uid
-       ,qid = req.body.qid;
+app.post('/qEr/joinQ', function (req, res){
+    var qer = {
+                cellNum:req.body.qernum
+                ,qMasCellNum:req.body.qmasnum
+            }
+
+/*************************Joining of a new Queue in MongoDB (works well)************************/
+    db.qEr.update({cellNum: qer.cellNum}, {$push: {joinedQs: qer.qMasCellNum}}, {multi: false} , function (err, doc) {
+        if (err){
+            console.log("err");
+        }
+        else{
+            console.log("New queue successfully joined: ");
+            res.json(doc);
+        }
+    });
+
+    var uid = qer.cellNum,
+        qid = qer.qMasCellNum;
+
     qEr.joinQ(uid, qid, function (param){
         console.log("QEr- '" + uid + "' joined Q: '" + qid + "'!");
         res.send(param);
     });
 
 });
-//CHECKED!!!
-app.post('/user/availQs', function (req, res){
-    qEr.allQs(function(param){
-    console.log("Available Qs are: '" + param + "'...")
+
+app.post('/qEr/availQs', function (req, res){
+
+/*******Listing of available qs in MongoDB (server side works well-lient side needs work)***********/
+    db.queues.find({"qActive": "true"}, function (err, doc) {
+        if (err){
+            console.log("err");
+        }
+        else{
+            console.log("queues that are active : " + doc);
+            res.json(doc);
+        }
+    });
+
+    qEr.allQsInProvider(function(param){
+        console.log("Available Qs are: '" + param + "'...")
         res.send(param);
-    console.log(param);
+        console.log(param);
     });
 });
 
 
-app.post('/user/categories', function (req, res){
+app.post('/qEr/categories', function (req, res){
     var uid = req.body.uid
        ,qid = req.body.qid;
     qEr.getCategories(function (param){
@@ -132,18 +227,41 @@ app.post('/user/categories', function (req, res){
 
 });
 
-app.post('/user/leaveQ', function (req, res){
+app.post('/qEr/leaveQ', function (req, res){
     var uid = req.body.uid
         ,qid = req.body.qid;
+
+/*************************Leaving a queue in MongoDB (works well)************************/
+    db.qEr.update({cellNum: uid}, {$pull: {"joinedQs": qid}}, function (err, doc) {
+        if (err){
+            console.log("err");
+        }
+        else{
+            console.log("queue successfully exited : ");
+            res.json(doc);
+        }
+    });
 
     qEr.leaveQ(uid, qid, function (param){
         console.log("QEr:'" + uid + "' has left Q:" + qid + "...");
         res.send(param);
     })
 });
-//CHECKED!!!
-app.post('/user/joinedQs', function (req, res){
-    var uid = req.body.uid;
+
+app.post('/qEr/joinedQs', function (req, res){
+    var uid = req.body.uid,
+        joinedqs = [];
+    /*for(var i = 0; i < 5; i++) {
+        db.qEr.find({"joinedQs": "true"}, function (err, doc) {
+            if (err){
+                console.log("err");
+            }
+            else{
+                console.log("queues that are active : ");
+                joinedqs.push(res.json(doc));
+            }
+        });
+    }*/
 
     qEr.joinedQ(uid, function (param){
         console.log("List of Qs joined by: " + uid + ":")
@@ -152,7 +270,7 @@ app.post('/user/joinedQs', function (req, res){
 });
 
 
-var server = app.listen(8080, function(){
+var server = app.listen(3000, function(){
     console.log("listening for Q requests on port 3000!");
 
 });
